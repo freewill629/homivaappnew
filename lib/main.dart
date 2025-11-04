e59ed172d7e47ab4618ef24b42059778198bc73d
VishnuDataNerd <vishnumohan321@gmail.com>
2025-11-04 10:29:06 +0530

    Guard Firebase initialization on startup

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,10 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyAoLf8Jvi4M5MMdCMjvZasl1uWuWlgeQhE',
-      authDomain: 'smartwatertank-3efdd.firebaseapp.com',
-      projectId: 'smartwatertank-3efdd',
-      storageBucket: 'smartwatertank-3efdd.firebasestorage.app',
-      messagingSenderId: '814599800607',
-      appId: '1:814599800607:web:bfcb3547eccf538294d22a',
-      databaseURL: 'https://smartwatertank-3efdd-default-rtdb.firebaseio.com',
-    ),
-  );
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(const HomivaApp());
 }
